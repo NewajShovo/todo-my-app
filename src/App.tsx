e5815edc6c271bd707ac4b366d201622a238bf51
NewajShovo <leoshovo@gmail.com>
2022-12-23 20:45:32 +0600

    css file added, label added, decoration file added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { type } from '@testing-library/user-event/dist/type';
 import React from 'react';
+import { TodoListItem } from './todolistItem';
+
+const todos: Array<Todo> = [
+  { text: "Walk the dog", complete: true },
+  { text: "Make app", complete: false }
+];
 
 function App() {
   return (
-    <div>Hello World</div>
+    <React.Fragment>
+      <TodoListItem todo={todos[0]} />
+      <TodoListItem todo={todos[1]} />
+    </React.Fragment>
   );
 }
 
